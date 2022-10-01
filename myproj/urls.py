3ada9462a5082f6e30e9c2e7ddd71f400b3a62d7
gaith mtiri <gaithmtiri@gmail.com>
2022-10-01 03:49:06 -0400

    dkd

--- myproj/urls.py
+++ myproj/urls.py
@@ -19,7 +19,8 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rest_app.urls'))
+    path('', include('rest_app.urls')),
+    path('users/', include('authenticate.urls')),
 ]
 
 # urlpatterns += [
