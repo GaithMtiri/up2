62fd9884b6b1a2e9f7372b3241d5ad55e6d34cc6
gaith mtiri <gaithmtiri@gmail.com>
2022-10-01 02:44:37 -0400

    dsd

--- myproj/settings.py
+++ myproj/settings.py
@@ -76,6 +76,13 @@ WSGI_APPLICATION = 'myproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
